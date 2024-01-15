1aa6406ef9316dbd7c253a806662d40f7a63fbbd
Tmdhoon2 <hks026naver@gmail.com>
2024-01-15 19:36:24 +0900

    chore :: gradle ktlint 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.jlleitschuh.gradle.ktlint)
+    //alias(libs.plugins.jlleitschuh.gradle.ktlint)
 }
