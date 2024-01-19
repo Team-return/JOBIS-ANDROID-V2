51262bc62bb59011a63ff9cc992031c39c7c250e
정승훈 <hks026naver@gmail.com>
2024-01-19 22:28:58 +0900

    chore :: remove kotlinOptions buildSrc build.gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
@@ -12,9 +10,4 @@ repositories {
 dependencies {
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.gradle)
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "17"
 }
