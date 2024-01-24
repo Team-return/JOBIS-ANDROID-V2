2bead217c28c9dcfba91b5422de52d8a6ac25a2b
Tmdhoon2 <hks026naver@gmail.com>
2024-01-24 16:47:17 +0900

    chroe :: create signup module

---
+++ feature/signup/build.gradle.kts
@@ -0,0 +1,12 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id(libs.plugins.androidLibrary.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+}
+
+apply<CommonGradlePlugin>()
+apply<ComposeGradlePlugin>()
+
+android {
+    namespace = "team.retum.signup"
+}
