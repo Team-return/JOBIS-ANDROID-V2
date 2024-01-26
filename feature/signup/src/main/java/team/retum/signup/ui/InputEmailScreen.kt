afe28ef47458952bc8ac82bcd54784e1171ba042
uiel <parkuiery@gmail.com>
2024-01-26 18:55:20 +0900

    refactor :: InputEmailScteen

--- feature/signup/src/main/java/team/retum/signup/ui/InputEmailScreen.kt
+++ feature/signup/src/main/java/team/retum/signup/ui/InputEmailScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,7 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import team.retum.signup.R
