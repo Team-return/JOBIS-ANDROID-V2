0ac211b2f4a078cce2a984239e8b0a9c1bd95b91
Tmdhoon2 <hks026naver@gmail.com>
2024-02-04 19:25:26 +0900

    refactor :: unused import 제거

--- app/src/main/java/team/retum/jobisandroidv2/ui/JobisApp.kt
+++ app/src/main/java/team/retum/jobisandroidv2/ui/JobisApp.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import team.retum.jobisandroidv2.navigation.NAVIGATION_AUTH
-import team.retum.jobisandroidv2.navigation.NAVIGATION_MAIN
 import team.retum.jobisandroidv2.navigation.authNavigation
 import team.retum.jobisandroidv2.navigation.mainNavigation
 
