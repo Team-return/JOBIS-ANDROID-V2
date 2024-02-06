2ab1efa7565b80f009b7cd0371b00e5e7288f3b1
rladmsdh <a01051355543@gmail.com>
2024-02-06 16:25:19 +0900

    feat :: alarm모듈 추가

---
+++ feature/alarm/build.gradle.kts
@@ -0,0 +1,20 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id(libs.plugins.android.library.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+    id(libs.plugins.hilt.android.get().pluginId)
+    id(libs.plugins.kotlin.kapt.get().pluginId)
+}
+
+apply<CommonGradlePlugin>()
+apply<ComposeGradlePlugin>()
+
+android {
+    namespace = "team.retum.alarm"
+}
+
+dependencies {
+
+    implementation(project(":core:common"))
+    implementation(project(":core:domain"))
+}
