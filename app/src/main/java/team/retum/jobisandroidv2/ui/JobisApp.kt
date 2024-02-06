bfd1fb055521ea16d1495c1047cab37c81be4648
rladmsdh <a01051355543@gmail.com>
2024-02-06 17:33:42 +0900

    refactor :: start 변경

--- app/src/main/java/team/retum/jobisandroidv2/ui/JobisApp.kt
+++ app/src/main/java/team/retum/jobisandroidv2/ui/JobisApp.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import team.retum.jobisandroidv2.navigation.NAVIGATION_AUTH
-import team.retum.jobisandroidv2.navigation.NAVIGATION_MAIN
 import team.retum.jobisandroidv2.navigation.authNavigation
 import team.retum.jobisandroidv2.navigation.mainNavigation
 
@@ -20,7 +19,7 @@ internal fun JobisApp() {
             .navigationBarsPadding()
             .statusBarsPadding(),
         navController = navController,
-        startDestination = NAVIGATION_MAIN,
+        startDestination = NAVIGATION_AUTH,
     ) {
         authNavigation(navController = navController)
         mainNavigation(navHostController = navController)
