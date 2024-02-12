89f424ca40cf2ad448b1e253de188ca8cd64a933
Tmdhoon2 <hks026naver@gmail.com>
2024-02-12 19:28:33 +0900

    feat :: DevelopmentArea enum 추가

---
+++ core/common/src/main/java/team/retum/common/enums/DevelopmentArea.kt
@@ -0,0 +1,9 @@
+package team.retum.common.enums
+
+enum class DevelopmentArea(val value: String) {
+    ALL(value = "전체"),
+    SERVER(value = "Server"),
+    WEB(value = "Web"),
+    ANDROID(value = "Android"),
+    IOS(value = "iOS"),
+}
