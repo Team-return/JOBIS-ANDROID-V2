3101e66e4fdc538aa6785c8285eeb37de7b040a9
yang-yunho <yangyoonho0710@naver.com>
2024-02-16 03:11:10 +0900

    refactor :: 마지막줄 개행 추가

--- feature/mypage/src/main/java/team/returm/mypage/navigation/MyPageNavigation.kt
+++ feature/mypage/src/main/java/team/returm/mypage/navigation/MyPageNavigation.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import team.returm.mypage.ui.MyPage
 
-
 const val NAVIGATION_MYPAGE = "mypage"
 
 fun NavGraphBuilder.mypage() {
