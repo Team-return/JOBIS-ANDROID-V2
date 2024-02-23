6e9be705a827efdf0d54c5bf57066bab20e6d055
Tmdhoon2 <hks026naver@gmail.com>
2024-02-23 18:44:57 +0900

    feat :: UserDataSource -> RemoteUserDataSource로 변경

--- core/network/src/main/java/team/retum/network/datasource/UserDataSource.kt
+++
@@ -1,8 +0,0 @@
-package team.retum.network.datasource
-
-import team.retum.network.model.request.SignInRequest
-import team.retum.network.model.response.TokenResponse
-
-interface UserDataSource {
-    suspend fun signIn(signInRequest: SignInRequest): TokenResponse
-}
