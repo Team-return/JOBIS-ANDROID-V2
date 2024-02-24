cd0baadb7aa1d1880932e1a78c93d161d91415a2
Tmdhoon2 <hks026naver@gmail.com>
2024-02-24 19:16:30 +0900

    feat :: TimerUtil

---
+++ core/common/src/main/java/team/retum/common/utils/TimerUtil.kt
@@ -0,0 +1,43 @@
+package team.retum.common.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+private const val MAX_TIMER_SECONDS = 300
+
+class TimerUtil {
+
+    private val _remainTime: MutableStateFlow<String> = MutableStateFlow("5:00")
+    val remainTime = _remainTime.asStateFlow()
+
+    private lateinit var timer: Job
+
+    init {
+        setTimer()
+    }
+
+    fun startTimer() {
+        timer.start()
+    }
+
+    fun stopTimer() {
+        timer.cancel()
+    }
+
+    fun setTimer() {
+        timer = CoroutineScope(Dispatchers.IO).launch {
+            repeat(MAX_TIMER_SECONDS) {
+                val remainTime = MAX_TIMER_SECONDS - it - 1
+                val minute = remainTime / 60
+                val second = remainTime - minute * 60
+                _remainTime.emit("0$minute:$second")
+                delay(1000L)
+            }
+        }
+    }
+}
