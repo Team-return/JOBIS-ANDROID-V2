94690e2ce833d83703ff31d2136096fefd7173d8
uiel <parkuiery@gmail.com>
2024-03-04 23:44:01 +0900

    feat :: 모집의뢰서 상세 viewModel

---
+++ feature/recruitment/src/main/java/team/retum/jobis/recruitment/viewmodel/RecruitmentDetailsViewModel.kt
@@ -0,0 +1,76 @@
+package team.retum.jobis.recruitment.viewmodel
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import team.retum.common.base.BaseViewModel
+import team.retum.common.utils.ResourceKeys
+import team.retum.usecase.entity.RecruitmentDetailsEntity
+import team.retum.usecase.usecase.bookmark.BookmarkRecruitmentUseCase
+import team.retum.usecase.usecase.recruitment.FetchRecruitmentDetailsUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+internal class RecruitmentDetailsViewModel @Inject constructor(
+    private val fetchRecruitmentDetailsUseCase: FetchRecruitmentDetailsUseCase,
+    private val bookmarkRecruitmentUseCase: BookmarkRecruitmentUseCase,
+) : BaseViewModel<RecruitmentDetailsState, Unit>(RecruitmentDetailsState.getDefaultState()) {
+
+    internal fun fetchRecruitmentDetails(recruitmentId: Long?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            fetchRecruitmentDetailsUseCase(recruitmentId = recruitmentId!!)
+                .onSuccess { detail ->
+                    with(detail) {
+                        setState {
+                            state.value.copy(
+                                recruitmentDetailsEntity = copy(
+                                    companyProfileUrl = ResourceKeys.IMAGE_URL + companyProfileUrl,
+                                ),
+                            )
+                        }
+                    }
+                }
+        }
+    }
+
+    internal fun bookmarkRecruitmentDetail(recruitmentId: Long) {
+        val entity = state.value.recruitmentDetailsEntity
+        val bookmarked = state.value.recruitmentDetailsEntity.bookmarked
+        setState { state.value.copy(recruitmentDetailsEntity = entity.copy(bookmarked = !bookmarked)) }
+        viewModelScope.launch(Dispatchers.IO) {
+            bookmarkRecruitmentUseCase(recruitmentId)
+        }
+    }
+}
+
+internal data class RecruitmentDetailsState(
+    val companyId: Long,
+    val recruitmentDetailsEntity: RecruitmentDetailsEntity,
+) {
+    companion object {
+        fun getDefaultState() = RecruitmentDetailsState(
+            companyId = 0,
+            recruitmentDetailsEntity = RecruitmentDetailsEntity(
+                companyProfileUrl = "",
+                companyName = "",
+                areas = emptyList(),
+                requiredGrade = null,
+                startTime = "",
+                endTime = "",
+                requiredLicenses = null,
+                hiringProgress = emptyList(),
+                trainPay = 0,
+                pay = null,
+                benefits = null,
+                military = false,
+                submitDocument = "",
+                startDate = "",
+                endDate = "",
+                etc = null,
+                isApplicable = false,
+                bookmarked = false,
+            ),
+        )
+    }
+}
