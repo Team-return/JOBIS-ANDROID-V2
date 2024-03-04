e2777b433204cadec2ecf8496192191bb29ce68b
uiel <parkuiery@gmail.com>
2024-03-04 23:01:18 +0900

    refactor :: 모집의뢰서 상세 네비게이션 수정

--- feature/recruitment/src/main/java/team/retum/jobis/recruitment/navigation/RecruitmentDetailsNavigation.kt
+++ feature/recruitment/src/main/java/team/retum/jobis/recruitment/navigation/RecruitmentDetailsNavigation.kt
@@ -2,24 +2,30 @@ package team.retum.jobis.recruitment.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import team.retum.jobis.recruitment.ui.RecruitmentDetails
 
 const val NAVIGATION_RECRUITMENT_DETAILS = "recruitmentDetails/"
-const val RECRUITMENT_ID = "{recruitment-id}"
+const val RECRUITMENT_ID = "recruitment-id"
 
 fun NavGraphBuilder.recruitmentDetails(
     onBackPressed: () -> Unit,
     onApplyClick: () -> Unit,
 ) {
-    composable(NAVIGATION_RECRUITMENT_DETAILS + RECRUITMENT_ID) {
+    composable(
+        route = "$NAVIGATION_RECRUITMENT_DETAILS{${RECRUITMENT_ID}}",
+        arguments = listOf(navArgument(RECRUITMENT_ID) { NavType.StringType }),
+    ) {
         RecruitmentDetails(
+            recruitmentId = it.arguments?.getString(RECRUITMENT_ID)?.toLong(),
             onBackPressed = onBackPressed,
             onApplyClick = onApplyClick,
         )
     }
 }
 
-fun NavController.navigateToRecruitmentDetails(recruitmentId: Long) {
+fun NavController.navigateToRecruitmentDetails(recruitmentId: Long?) {
     navigate(NAVIGATION_RECRUITMENT_DETAILS + recruitmentId)
 }
