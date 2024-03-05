5787e037ae4c8da06f99d22a247e598d8e257529
rladmsdh <a01051355543@gmail.com>
2024-03-05 13:59:01 +0900

    feat :: 리뷰 작성

--- core/domain/src/main/java/team/retum/usecase/entity/PostReviewEntity.kt
+++ core/domain/src/main/java/team/retum/usecase/entity/PostReviewEntity.kt
@@ -13,13 +13,13 @@ data class PostReviewEntity(
     )
 }
 
-fun PostReviewRequest.toPostReviewEntity() = PostReviewEntity(
+fun PostReviewEntity.toPostReviewRequest() = PostReviewRequest(
     companyId = this.companyId,
-    qnaElements = this.qnaElements.map { it.toEntity() }
+    qnaElements = this.qnaElements.map { it.toEntity() },
 )
 
-private fun PostReviewRequest.PostReviewContentRequest.toEntity() =
-    PostReviewEntity.PostReviewContentEntity(
+private fun PostReviewEntity.PostReviewContentEntity.toEntity() =
+    PostReviewRequest.PostReviewContentRequest(
         question = this.question,
         answer = this.answer,
         codeId = this.codeId,
--- core/domain/src/main/java/team/retum/usecase/usecase/review/PostReviewUseCase.kt
+++ core/domain/src/main/java/team/retum/usecase/usecase/review/PostReviewUseCase.kt
@@ -1,13 +1,14 @@
 package team.retum.usecase.usecase.review
 
-import team.retum.data.repository.ReviewRepository
-import team.retum.network.model.request.PostReviewRequest
+import team.retum.data.repository.review.ReviewRepository
+import team.retum.usecase.entity.PostReviewEntity
+import team.retum.usecase.entity.toPostReviewRequest
 import javax.inject.Inject
 
 class PostReviewUseCase @Inject constructor(
     private val postReviewRepository: ReviewRepository,
 ) {
-    suspend operator fun invoke(postReviewRequest: PostReviewRequest) = runCatching {
-        postReviewRepository.postReview(postReviewRequest)
+    suspend operator fun invoke(postReviewRequest: PostReviewEntity) = runCatching {
+        postReviewRepository.postReview(postReviewRequest.toPostReviewRequest())
     }
 }
