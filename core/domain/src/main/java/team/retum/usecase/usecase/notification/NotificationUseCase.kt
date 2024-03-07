451d7c7df78a46a49765725d58910926460d8aa4
uielPark <parkuiery@gmail.com>
2024-03-07 22:09:31 +0900

    feat :: NotificationUseCase

---
+++ core/domain/src/main/java/team/retum/usecase/usecase/notification/NotificationUseCase.kt
@@ -0,0 +1,14 @@
+package team.retum.usecase.usecase.notification
+
+import team.retum.data.repository.notification.NotificationRepository
+import javax.inject.Inject
+
+class NotificationUseCase @Inject constructor(
+    private val notificationRepository: NotificationRepository,
+) {
+    suspend operator fun invoke(
+        notificationId: Long,
+    ) = runCatching {
+        notificationRepository.fetchNotificationRead(notificationId = notificationId)
+    }
+}
