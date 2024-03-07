9f68a5d5e466239640abbfa4c53f960f760ac31b
uielPark <parkuiery@gmail.com>
2024-03-07 22:10:58 +0900

    feat :: NotificationDataSource

---
+++ core/network/src/main/java/team/retum/network/datasource/notification/NotificationDataSource.kt
@@ -0,0 +1,8 @@
+package team.retum.network.datasource.notification
+
+import team.retum.network.model.response.notification.FetchNotificationsResponse
+
+interface NotificationDataSource {
+    suspend fun fetchNotifications(isNew: Boolean) : FetchNotificationsResponse
+    suspend fun fetchNotificationRead(notificationId: Long)
+}
