aa1adf521d273173c48f68c2d89d8021b63213b7
Tmdhoon2 <hks026naver@gmail.com>
2024-03-08 22:54:17 +0900

    refactor :: unused import 제거

--- app/src/main/java/team/retum/jobisandroidv2/root/RootNavigation.kt
+++ app/src/main/java/team/retum/jobisandroidv2/root/RootNavigation.kt
@@ -4,7 +4,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import team.retum.jobis.splash.navigation.NAVIGATION_SPLASH
-import team.retum.landing.navigation.NAVIGATION_LANDING
 
 const val NAVIGATION_ROOT = "root"
 
