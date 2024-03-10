06501c77d1ad654acb7748fc5241b20a61e4faa1
rladmsdh <a01051355543@gmail.com>
2024-03-10 19:52:08 +0900

    refactor :: skill 파라미터 변경

--- core/common/src/main/java/team/retum/common/component/Skills.kt
+++ core/common/src/main/java/team/retum/common/component/Skills.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -20,16 +19,17 @@ import team.retum.jobisdesignsystemv2.utils.clickable
 @Composable
 fun Skills(
     skills: SnapshotStateList<String>,
+    checkSkillsId: List<Long>? = null,
     checkedSkills: List<String>,
-    onCheckedChange: (String, Boolean) -> Unit,
+    onCheckedChange: (String, Boolean, Long) -> Unit,
 ) {
     LazyColumn {
-        items(skills) { skill ->
-            val checked = checkedSkills.contains(skill)
+        items(skills.size) { skill ->
+            val checked = checkedSkills.contains(skills[skill])
             SkillContent(
-                skill = skill,
+                skill = skills[skill],
                 checked = checked,
-                onClick = { onCheckedChange(skill, it) },
+                onClick = { onCheckedChange(skills[skill], it, checkSkillsId?.get(skill) ?: 0) },
             )
         }
     }
