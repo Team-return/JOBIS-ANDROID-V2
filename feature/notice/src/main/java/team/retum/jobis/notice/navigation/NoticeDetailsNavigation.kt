042df066baba5eed75a90d50a3c551cbf08203bb
uielPark <parkuiery@gmail.com>
2024-03-10 22:21:15 +0900

    refactor :: arguments 추가

--- feature/notice/src/main/java/team/retum/jobis/notice/navigation/NoticeDetailsNavigation.kt
+++ feature/notice/src/main/java/team/retum/jobis/notice/navigation/NoticeDetailsNavigation.kt
@@ -2,17 +2,25 @@ package team.retum.jobis.notice.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import team.retum.jobis.notice.ui.NoticeDetails
 
 const val NAVIGATION_NOTICE_DETAILS = "noticeDetails/"
-const val NOTICE_ID = "{notice-id}"
+const val NOTICE_ID = "notice-id"
 
 fun NavGraphBuilder.noticeDetails(
     onBackPressed: () -> Unit,
 ) {
-    composable(NAVIGATION_NOTICE_DETAILS + NOTICE_ID) {
-        NoticeDetails(onBackPressed = onBackPressed)
+    composable(
+        route = "NAVIGATION_NOTICE_DETAILS{$NOTICE_ID}",
+        arguments = listOf(navArgument(NOTICE_ID) { NavType.StringType }),
+    ) {
+        NoticeDetails(
+            onBackPressed = onBackPressed,
+            noticeId = it.arguments?.getString(NOTICE_ID)?.toLong() ?: throw NullPointerException(),
+        )
     }
 }
 
