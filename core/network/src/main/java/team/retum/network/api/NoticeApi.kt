1e855d862cfbfacadef9c67e4629ef8f7be0f74b
uielPark <parkuiery@gmail.com>
2024-03-10 22:18:59 +0900

    refactor :: fetchNoticeDetails

--- core/network/src/main/java/team/retum/network/api/NoticeApi.kt
+++ core/network/src/main/java/team/retum/network/api/NoticeApi.kt
@@ -1,13 +1,20 @@
 package team.retum.network.api
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 import team.retum.network.di.RequestUrls
+import team.retum.network.model.response.notice.FetchNoticeDetailsResponse
 import team.retum.network.model.response.notice.FetchNoticesResponse
 
 interface NoticeApi {
     @GET(RequestUrls.Notice.notices)
     suspend fun fetchNotices(
         @Query("page") page: Int,
-    ) : FetchNoticesResponse
+    ): FetchNoticesResponse
+
+    @GET(RequestUrls.Notice.details)
+    suspend fun fetchNoticeDetails(
+        @Path("notice-id") noticeId: Long,
+    ): FetchNoticeDetailsResponse
 }
