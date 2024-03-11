8220a9aea14e0a99f0f637b1675955dd8dc5505e
uiel <parkuiery@gmail.com>
2024-03-11 22:45:59 +0900

    refactor :: 상세 및 페이징 처리 repository

--- core/data/src/main/java/team/retum/data/repository/notice/NoticeRepository.kt
+++ core/data/src/main/java/team/retum/data/repository/notice/NoticeRepository.kt
@@ -1,7 +1,11 @@
 package team.retum.data.repository.notice
 
+import team.retum.network.model.response.notice.FetchNoticeDetailsResponse
+import team.retum.network.model.response.notice.FetchNoticePageCountResponse
 import team.retum.network.model.response.notice.FetchNoticesResponse
 
 interface NoticeRepository {
     suspend fun fetchNotices(page: Int): FetchNoticesResponse
+    suspend fun fetchNoticeDetails(noticeId: Long): FetchNoticeDetailsResponse
+    suspend fun fetchPageCount(title: String?): FetchNoticePageCountResponse
 }
