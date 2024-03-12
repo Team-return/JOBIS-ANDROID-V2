f6062f1f94a960bf94ab042732a217907aa73d99
uielPark <parkuiery@gmail.com>
2024-03-13 08:17:37 +0900

    refactor :: 예외 처리 추가

--- feature/notice/src/main/java/team/retum/jobis/notice/ui/NoticeDetailsScreen.kt
+++ feature/notice/src/main/java/team/retum/jobis/notice/ui/NoticeDetailsScreen.kt
@@ -14,19 +14,23 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import team.retum.jobis.notice.viewmodel.NoticeDetailsSideEffect
 import team.retum.jobis.notice.viewmodel.NoticeDetailsState
 import team.retum.jobis.notice.viewmodel.NoticeDetailsViewModel
 import team.retum.jobis.notification.R
 import team.retum.jobisdesignsystemv2.appbar.JobisSmallTopAppBar
 import team.retum.jobisdesignsystemv2.button.JobisIconButton
+import team.retum.jobisdesignsystemv2.foundation.JobisIcon
 import team.retum.jobisdesignsystemv2.foundation.JobisTheme
 import team.retum.jobisdesignsystemv2.foundation.JobisTypography
 import team.retum.jobisdesignsystemv2.text.JobisText
+import team.retum.jobisdesignsystemv2.toast.JobisToast
 import team.retum.usecase.entity.notice.NoticeDetailsEntity
 
 
@@ -37,9 +41,21 @@ internal fun NoticeDetails(
     noticeDetailsViewModel: NoticeDetailsViewModel = hiltViewModel(),
 ) {
     val state by noticeDetailsViewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     LaunchedEffect(Unit) {
         noticeDetailsViewModel.fetchNoticeDetails(noticeId = noticeId)
+        noticeDetailsViewModel.sideEffect.collect {
+            when (it) {
+                is NoticeDetailsSideEffect.BadRequest -> {
+                    JobisToast.create(
+                        context = context,
+                        message = context.getString(R.string.occurred_error),
+                        drawable = JobisIcon.Error,
+                    )
+                }
+            }
+        }
     }
 
     NoticeDetailsScreen(
