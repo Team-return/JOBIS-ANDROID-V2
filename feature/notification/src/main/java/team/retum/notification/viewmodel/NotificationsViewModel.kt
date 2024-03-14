7539bfdace4127e04e08c105b7265f039916bdb9
uiel <parkuiery@gmail.com>
2024-03-14 23:23:58 +0900

    refactor :: 페이지 이동 처리 추가

--- feature/notification/src/main/java/team/retum/notification/viewmodel/NotificationsViewModel.kt
+++ feature/notification/src/main/java/team/retum/notification/viewmodel/NotificationsViewModel.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import team.retum.common.base.BaseViewModel
+import team.retum.common.enums.AlarmType
 import team.retum.usecase.entity.notification.NotificationsEntity
 import team.retum.usecase.usecase.notification.FetchNotificationsUseCase
 import team.retum.usecase.usecase.notification.ReadNotificationUseCase
@@ -18,8 +19,8 @@ internal class NotificationsViewModel @Inject constructor(
     private val readNotificationUseCase: ReadNotificationUseCase,
 ) : BaseViewModel<NotificationsState, NotificationsSideEffect>(NotificationsState.getDefaultState()) {
 
-     private val _notifications: SnapshotStateList<NotificationsEntity.NotificationEntity> =
-         mutableStateListOf()
+    private val _notifications: SnapshotStateList<NotificationsEntity.NotificationEntity> =
+        mutableStateListOf()
     val notifications get() = _notifications
 
     internal fun fetchNotifications() {
@@ -34,10 +35,28 @@ internal class NotificationsViewModel @Inject constructor(
         }
     }
 
-    internal fun readNotification(notificationId: Long) {
+    internal fun readNotification(
+        isNew: Boolean,
+        notificationId: Long,
+        detailId: Long,
+        topic: AlarmType,
+    ) {
         viewModelScope.launch(Dispatchers.IO) {
-            readNotificationUseCase(notificationId)
-            postSideEffect(NotificationsSideEffect.MoveToDetail(notificationId = notificationId))
+            if (isNew) readNotificationUseCase(notificationId)
+            when (topic) {
+                AlarmType.APPLICATION_STATUS_CHANGED -> {
+                    postSideEffect(NotificationsSideEffect.MoveToHome(applicationId = detailId))
+
+                }
+
+                AlarmType.RECRUITMENT_DONE -> {
+                    postSideEffect(NotificationsSideEffect.MoveToRecruitment(recruitmentId = detailId))
+                }
+
+                else -> {
+
+                }
+            }
         }
     }
 
@@ -63,5 +82,6 @@ internal data class NotificationsState(
 }
 
 internal sealed interface NotificationsSideEffect {
-    data class MoveToDetail(val notificationId: Long) : NotificationsSideEffect
+    data class MoveToRecruitment(val recruitmentId: Long) : NotificationsSideEffect
+    data class MoveToHome(val applicationId: Long) : NotificationsSideEffect
 }
