f6aa115e003bfcb9ffe752be3c380cdb9a616f4e
uiel <parkuiery@gmail.com>
2024-03-14 22:39:57 +0900

    refactor :: 파라미터 추가

--- app/src/main/java/team/retum/jobisandroidv2/root/RootNavigation.kt
+++ app/src/main/java/team/retum/jobisandroidv2/root/RootNavigation.kt
@@ -40,7 +40,7 @@ fun NavGraphBuilder.root(
     }
 }
 
-fun NavController.navigateToRoot() {
+fun NavController.navigateToRoot(applicationId: Long? = 0) {
     navigate(NAVIGATION_ROOT) {
         popUpTo(NAVIGATION_SPLASH) {
             inclusive = true
