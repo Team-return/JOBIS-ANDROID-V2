a44f9e35dd5a542b631062d615d78369d49edc19
Tmdhoon2 <hks026naver@gmail.com>
2024-04-07 13:21:05 +0900

    refactor :: CompanyItems 함수 추상화

--- feature/company/src/main/java/team/retum/company/component/CompanyItems.kt
+++ feature/company/src/main/java/team/retum/company/component/CompanyItems.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,6 +24,7 @@ import team.retum.jobisdesignsystemv2.foundation.JobisTheme
 import team.retum.jobisdesignsystemv2.foundation.JobisTypography
 import team.retum.jobisdesignsystemv2.text.JobisText
 import team.retum.jobisdesignsystemv2.utils.clickable
+import team.retum.usecase.entity.CompaniesEntity
 import team.retum.usecase.entity.HasRecruitment
 
 private const val DEFAULT_SIZE_COMPANY_NAME = 1f
@@ -29,7 +32,36 @@ private const val DEFAULT_SIZE_HAS_RECRUITMENT = 0.6f
 private const val DEFAULT_SIZE_TAKE = 0.5f
 
 @Composable
-internal fun CompanyItem(
+internal fun CompanyItems(
+    companies: List<CompaniesEntity.CompanyEntity>,
+    onCompanyContentClick: (Long) -> Unit,
+    whetherFetchNextPage: (lastVisibleItemIndex: Int) -> Boolean,
+    fetchNextPage: () -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .background(JobisTheme.colors.background),
+    ) {
+        itemsIndexed(companies) { index, item ->
+            CompanyItem(
+                onCompanyContentClick = onCompanyContentClick,
+                companyImageUrl = item.logoUrl,
+                companyName = item.name,
+                id = item.id,
+                take = item.take,
+                hasRecruitment = item.hasRecruitment,
+            )
+            if (whetherFetchNextPage(index)) {
+                fetchNextPage()
+            }
+        }
+    }
+}
+
+@Composable
+private fun CompanyItem(
     modifier: Modifier = Modifier,
     onCompanyContentClick: (Long) -> Unit,
     companyImageUrl: String,
