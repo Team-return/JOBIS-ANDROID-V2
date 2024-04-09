43c602fb8a3f393813e87f94a403d09a09eca5d1
rladmsdh <a01051355543@gmail.com>
2024-04-09 15:19:37 +0900

    test :: 컴포즈 함수 수명주기 테스트

---
+++ core/common/src/main/java/team/retum/common/utils/ComposableLifecycle.kt
@@ -0,0 +1,26 @@
+package team.retum.common.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+@Composable
+fun ComposableLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    onEvent: (LifecycleOwner, Lifecycle.Event) -> Unit,
+) {
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { source, event ->
+            onEvent(source, event)
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            println(Lifecycle.Event.ON_DESTROY)
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
