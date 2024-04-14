b7837a8b4108deaa58e1534fe8f63db1f2c7c895
Tmdhoon2 <hks026naver@gmail.com>
2024-04-14 14:47:52 +0900

    chore :: v2.0.6

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -2,8 +2,8 @@ object ProjectProperties {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 28
     const val TARGET_SDK = 34
-    const val VERSION_CODE = 7
-    const val VERSION_NAME = "2.0.5"
+    const val VERSION_CODE = 8
+    const val VERSION_NAME = "2.0.6"
     const val COMPOSE_COMPILER_EXTENSION = "1.5.6"
     const val JVM_TARGET = "18"
 }
