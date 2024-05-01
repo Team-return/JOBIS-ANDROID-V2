5ea953dda3c022e58c9f70dd75f5381500168e32
Tmdhoon2 <hks026naver@gmail.com>
2024-05-01 10:03:58 +0900

    feat :: SignInTestActivity

---
+++ feature/signin/src/main/java/team/retum/signin/SignInTestActivity.kt
@@ -0,0 +1,7 @@
+package team.retum.signin
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignInTestActivity: ComponentActivity()
