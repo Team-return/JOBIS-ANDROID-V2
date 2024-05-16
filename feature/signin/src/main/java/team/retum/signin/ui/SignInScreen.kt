a55f02e4f3d43b525a3745e38704dfc996e6a785
rladmsdh <a01051355543@gmail.com>
2024-05-16 22:21:02 +0900

    fix :: lint

--- feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
+++ feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
