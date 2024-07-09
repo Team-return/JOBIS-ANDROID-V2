83fdff138ee6dfaee154e463a03dc092e28cc439
Tmdhoon2 <hks026naver@gmail.com>
2024-07-09 14:16:06 +0900

    refactor :: SignInScreen unused import 제거

--- feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
+++ feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
