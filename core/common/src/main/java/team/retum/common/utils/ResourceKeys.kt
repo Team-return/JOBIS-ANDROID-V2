fb854063727668c38cc8431e52a602665068f7c6
uiel <parkuiery@gmail.com>
2024-07-22 17:55:21 +0900

    refactor :: s3 url 변경

--- core/common/src/main/java/team/retum/common/utils/ResourceKeys.kt
+++ core/common/src/main/java/team/retum/common/utils/ResourceKeys.kt
@@ -6,7 +6,7 @@ object ResourceKeys {
     const val REFRESH_TOKEN = "refreshToken"
     const val REFRESH_EXPIRES_AT = "refreshExpiresAt"
     const val DEVICE_TOKEN = "deviceToken"
-    const val IMAGE_URL = "https://jobis-store.s3.ap-northeast-2.amazonaws.com/"
+    const val IMAGE_URL = "https://dsm-s3-bucket-jobis.s3.ap-northeast-2.amazonaws.com/"
     const val SHARED_PREFERENCES_NAME = "jobis"
     const val BEARER = "Bearer"
     const val SIGN_UP = "signUp"
