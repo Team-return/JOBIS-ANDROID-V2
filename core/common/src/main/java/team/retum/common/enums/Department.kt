ed0335294e89aed216babbdd221f7c1a3faf8abd
uiel <parkuiery@gmail.com>
2024-08-04 23:44:32 +0900

    ♻️ :: 학과 이름 변경

--- core/common/src/main/java/team/retum/common/enums/Department.kt
+++ core/common/src/main/java/team/retum/common/enums/Department.kt
@@ -2,7 +2,7 @@ package team.retum.common.enums
 
 enum class Department(val value: String) {
     SOFTWARE_DEVELOP("소프트웨어개발과"),
-    EMBEDDED_SOFTWARE("임베디소프트웨어과"),
+    EMBEDDED_SOFTWARE("임베디드소프트웨어과"),
     INFORMATION_SECURITY("정보보안과"),
     AI_SOFTWARE("인공지능소프트웨어과"),
 }
