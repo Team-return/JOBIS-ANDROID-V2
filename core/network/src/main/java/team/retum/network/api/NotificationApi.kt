99eb6aab88e28c053e93db9b14bafdf4b1609475
uiel <parkuiery@gmail.com>
2024-08-18 22:51:35 +0900

    feat :: 알림 구독 및 취소 api 추가

--- core/network/src/main/java/team/retum/network/api/NotificationApi.kt
+++ core/network/src/main/java/team/retum/network/api/NotificationApi.kt
@@ -1,9 +1,12 @@
 package team.retum.network.api
 
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import team.retum.common.enums.NotificationTopic
 import team.retum.network.di.RequestUrls
 import team.retum.network.model.response.notification.FetchNotificationsResponse
 
@@ -17,5 +20,17 @@ interface NotificationApi {
     @PATCH(RequestUrls.Notification.notification)
     suspend fun readNotification(
         @Path("notification-id") notificationId: Long,
+    )
+
+    @POST(RequestUrls.Notification.topic)
+    suspend fun subscribeNotificationTopic(
+        @Query("token") deviceToken: String,
+        @Query("topic") topic: NotificationTopic,
+    )
+
+    @DELETE(RequestUrls.Notification.topic)
+    suspend fun unsubscribeNotificationTopic(
+        @Query("token") deviceToken: String,
+        @Query("topic") topic: NotificationTopic,
     )
 }
