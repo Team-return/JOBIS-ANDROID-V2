5cc0412fb176c4c8623c821fa026a9fad9add977
uiel <parkuiery@gmail.com>
2024-08-18 21:18:05 +0900

    refactor :: 알림 구독 및 취소 추가

--- core/data/src/main/java/team/retum/data/repository/notification/NotificationRepository.kt
+++ core/data/src/main/java/team/retum/data/repository/notification/NotificationRepository.kt
@@ -1,8 +1,20 @@
 package team.retum.data.repository.notification
 
+import team.retum.common.enums.NotificationTopic
 import team.retum.network.model.response.notification.FetchNotificationsResponse
 
 interface NotificationRepository {
     suspend fun fetchNotifications(isNew: Boolean?): FetchNotificationsResponse
+
     suspend fun readNotification(notificationId: Long)
+
+    suspend fun subscribeNotificationTopic(
+        deviceToken: String,
+        topic: NotificationTopic,
+    )
+
+    suspend fun unsubscribeNotificationTopic(
+        deviceToken: String,
+        topic: NotificationTopic,
+    )
 }
--- core/data/src/main/java/team/retum/data/repository/notification/NotificationRepositoryImpl.kt
+++ core/data/src/main/java/team/retum/data/repository/notification/NotificationRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package team.retum.data.repository.notification
 
+import team.retum.common.enums.NotificationTopic
 import team.retum.network.datasource.notification.NotificationDataSource
 import team.retum.network.model.response.notification.FetchNotificationsResponse
 import javax.inject.Inject
@@ -12,4 +13,20 @@ class NotificationRepositoryImpl @Inject constructor(
 
     override suspend fun readNotification(notificationId: Long) =
         notificationDataSource.readNotification(notificationId = notificationId)
+
+    override suspend fun subscribeNotificationTopic(
+        deviceToken: String,
+        topic: NotificationTopic,
+    ) = notificationDataSource.subscribeNotificationTopic(
+        deviceToken = deviceToken,
+        topic = topic,
+    )
+
+    override suspend fun unsubscribeNotificationTopic(
+        deviceToken: String,
+        topic: NotificationTopic,
+    ) = notificationDataSource.unsubscribeNotificationTopic(
+        deviceToken = deviceToken,
+        topic = topic,
+    )
 }
