49502ae071dcd14d7790a454bba1b61733d86b38
uiel <parkuiery@gmail.com>
2024-09-06 12:20:58 +0900

    refactor :: import 제거

--- feature/bookmark/src/main/java/team/retum/bookmark/ui/BookmarkScreen.kt
+++ feature/bookmark/src/main/java/team/retum/bookmark/ui/BookmarkScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
