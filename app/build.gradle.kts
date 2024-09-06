986e5c4003eb0c2d336231fe866849131aff77c7
uiel <parkuiery@gmail.com>
2024-09-07 06:53:14 +0900

    build :: 파일 경로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("/home/runner/work/_temp/keystore/jobis_v2_key.jks")
+            storeFile = file("./keystore/jobis_v2_key.jks")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
