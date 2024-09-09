8cb76a060943ec390f35f6f1ee780ca8469bca1f
uiel <parkuiery@gmail.com>
2024-09-09 23:43:38 +0900

    build :: 버전 변경

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -3,7 +3,7 @@ object ProjectProperties {
     const val MIN_SDK = 28
     const val TARGET_SDK = 34
     const val VERSION_CODE = 14
-    const val VERSION_NAME = "2.1.8"
+    const val VERSION_NAME = "2.1.9"
     const val COMPOSE_COMPILER_EXTENSION = "1.5.6"
     const val JVM_TARGET = "18"
 }
