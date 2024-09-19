392a4d137a7e2c8a5d085410c2f75e810d6e0e18
uiel <parkuiery@gmail.com>
2024-09-19 16:00:49 +0900

    feat :: WinterInternNavigation 구현

---
+++ feature/recruitment/src/main/java/team/retum/jobis/recruitment/navigation/WinterInternNavigation.kt
@@ -0,0 +1,28 @@
+package team.retum.jobis.recruitment.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import team.retum.jobis.recruitment.ui.WinterIntern
+
+private const val NAVIGATION_WINTER_INTERN = "winterIntern"
+
+fun NavGraphBuilder.winterIntern(
+    onBackPressed: () -> Unit,
+    onRecruitmentDetailsClick: (Long) -> Unit,
+    onRecruitmentFilterClick: () -> Unit,
+    onSearchRecruitmentClick: () -> Unit,
+) {
+    composable(NAVIGATION_WINTER_INTERN) {
+        WinterIntern(
+            onBackPressed = onBackPressed,
+            onRecruitmentDetailsClick = onRecruitmentDetailsClick,
+            onRecruitmentFilterClick = onRecruitmentFilterClick,
+            onSearchRecruitmentClick = onSearchRecruitmentClick,
+        )
+    }
+}
+
+fun NavController.navigateToWinterIntern() {
+    navigate(NAVIGATION_WINTER_INTERN)
+}
