0511262faf530f388be84117256604b16ebe27ee
uiel <parkuiery@gmail.com>
2024-11-05 11:33:45 +0900

    refactor :: reissue 처리

--- core/network/src/main/java/team/retum/network/util/TokenInterceptor.kt
+++ core/network/src/main/java/team/retum/network/util/TokenInterceptor.kt
@@ -1,27 +1,16 @@
 package team.retum.network.util
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
-import okhttp3.RequestBody
 import okhttp3.Response
-import retrofit2.Retrofit
-import team.retum.common.enums.PlatformType
 import team.retum.common.utils.ResourceKeys
 import team.retum.jobis.local.datasource.user.LocalUserDataSource
-import team.retum.network.BuildConfig
-import team.retum.network.api.AuthApi
 import team.retum.network.di.RequestUrls
 import javax.inject.Inject
 
 class TokenInterceptor @Inject constructor(
     private val localUserDataSource: LocalUserDataSource,
-    //private val retrofit: Retrofit,
-    //private val authApi: AuthApi,
 ) : Interceptor {
-
-    //private lateinit var accessToken: String
 
     private val ignorePaths by lazy {
         listOf(
@@ -58,45 +47,27 @@ class TokenInterceptor @Inject constructor(
 
             val refreshToken = localUserDataSource.getRefreshToken()
 
-            val newAccessToken = RefreshTokenService.refreshToken(refreshToken)
-            if(newAccessToken != null) {
-//                request = request.newBuilder()
-//                    .url("${BuildConfig.BASE_URL}/auth/reissue?platform-type='Android'")
-//                    .header("X-Refresh-Token","")
-//                    .put(RequestBody.create(null,""))
-//                    .build()
+            runBlocking {
+                val newToken = RefreshTokenService.refreshToken(refreshToken)
+                localUserDataSource.run {
+                    saveAccessToken(newToken.accessToken)
+                    saveAccessExpiresAt(newToken.accessExpiresAt)
+                    saveRefreshToken(newToken.refreshToken)
+                    saveRefreshExpiresAt(newToken.refreshExpiresAt)
+                }
+
                 request = request.newBuilder()
-                    .removeHeader("X-Refresh-Token")
-                    .addHeader("X-Refresh-Token", "${ResourceKeys.BEARER} $newAccessToken")
+                    .removeHeader("Authorization")
+                    .addHeader("Authorization", "${ResourceKeys.BEARER} ${newToken.accessToken}")
                     .build()
 
                 response = chain.proceed(request)
             }
         }
-
         return response
     }
 
     private fun checkS3Request(url: String): Boolean {
         return url.contains(ResourceKeys.IMAGE_URL)
-    }
-
-    private fun reissueToken(): String? {
-        val refreshToken = localUserDataSource.getRefreshToken()
-
-        var token: String? = null
-        CoroutineScope(Dispatchers.IO).launch {
-//            runCatching {
-//                authApi.reissueToken(
-//                    refreshToken = refreshToken,
-//                    platformType = PlatformType.ANDROID,
-//                )
-//            }.onSuccess {
-//               token = it.accessToken
-//            }.onFailure {
-//                token = null
-//            }
-        }
-        return token
     }
 }
