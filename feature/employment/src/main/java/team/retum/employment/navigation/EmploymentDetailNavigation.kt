0b7733c2fb132a53c6d81275942fbcedd2e4ea3a
유섭 <yoonbk2001027@gmail.com>
2025-03-10 09:19:45 +0900

    feat :: employmentDetailNavigation 생성

---
+++ feature/employment/src/main/java/team/retum/employment/navigation/EmploymentDetailNavigation.kt
@@ -0,0 +1,30 @@
+package team.retum.employment.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import team.retum.common.utils.ResourceKeys
+import team.retum.employment.ui.EmploymentDetail
+
+const val EMPLOYMENT_DETAIL = "employmentDetail"
+
+fun NavGraphBuilder.employmentDetail(
+    onBackPressed: () -> Unit,
+) {
+    composable(
+        route = "$EMPLOYMENT_DETAIL/${ResourceKeys.CLASS_ID}",
+        arguments = listOf(navArgument(ResourceKeys.CLASS_ID) { type = NavType.StringType })
+    ) {
+        val classId = it.arguments?.getString(ResourceKeys.CLASS_ID) ?: ""
+        EmploymentDetail(
+            onBackPressed = onBackPressed,
+            classId = classId,
+        )
+    }
+}
+
+fun NavController.navigateToEmploymentDetail(classId: Long) {
+    navigate(EMPLOYMENT_DETAIL + classId)
+}
