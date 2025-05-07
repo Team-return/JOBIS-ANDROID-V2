000ab3d3930f94e3c950c8103d56991f819b5266
유섭 <yoonbk2001027@gmail.com>
2025-05-07 14:30:40 +0900

    build :: gradle 어노테이션 추가

--- feature/employment/build.gradle.kts
+++ feature/employment/build.gradle.kts
@@ -1,3 +1,4 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
