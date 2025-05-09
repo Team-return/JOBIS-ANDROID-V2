1603aade2b7c09f954a088cce3e8590c384f3c6c
유섭 <yoonbk2001027@gmail.com>
2025-05-09 09:04:27 +0900

    refactor :: 관심 분야 viewModel 기능 함수 구현

--- feature/interests/src/main/java/team/retum/jobis/interests/viewmodel/InterestsViewmodel.kt
+++ feature/interests/src/main/java/team/retum/jobis/interests/viewmodel/InterestsViewmodel.kt
@@ -1,6 +1,5 @@
 package team.retum.jobis.interests.viewmodel
 
-import android.util.Log
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,15 +7,21 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import team.retum.common.base.BaseViewModel
 import team.retum.usecase.entity.interests.InterestsEntity
+import team.retum.usecase.entity.interests.InterestsRecruitmentsEntity
+import team.retum.usecase.usecase.interests.FetchInterestsSearchRecruitmentsUseCase
 import team.retum.usecase.usecase.interests.FetchInterestsUseCase
+import team.retum.usecase.usecase.interests.SetInterestsToggleUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 internal class InterestsViewmodel @Inject constructor(
     private val fetchInterestsUseCase: FetchInterestsUseCase,
+    private val fetchInterestsSearchRecruitmentsUseCase: FetchInterestsSearchRecruitmentsUseCase,
+    private val setInterestsToggleUseCase: SetInterestsToggleUseCase,
 ) : BaseViewModel<InterestsState, InterestsSideEffect>(InterestsState.getInitialState()) {
     init {
         fetchInterests()
+        fetchInterestsSearchRecruitments()
     }
 
     private fun fetchInterests() {
@@ -24,23 +29,70 @@ internal class InterestsViewmodel @Inject constructor(
             fetchInterestsUseCase().onSuccess {
                 setState {
                     state.value.copy(
-                        interestsList = it,
+                        interestsMajorList = it,
                     )
                 }
             }
-            Log.d("fetchInterests", "fetchInterests: ${state.value.interestsList}")
+        }
+    }
+
+    private fun fetchInterestsSearchRecruitments() {
+        viewModelScope.launch(Dispatchers.IO) {
+            fetchInterestsSearchRecruitmentsUseCase().onSuccess {
+                setState {
+                    state.value.copy(
+                        interestsRecruitments = it,
+                    )
+                }
+            }
+        }
+    }
+
+    internal fun setInterestsToggle(codes: List<Int>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            setInterestsToggleUseCase(
+                codes = codes,
+            ).onSuccess {
+            }
+        }
+    }
+
+    internal fun setMajorId(majorId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            setState {
+                state.value.copy(
+                    majorId = majorId,
+                )
+            }
+        }
+    }
+
+    internal fun updateCodeIds(codeIds: List<Int>) {
+        viewModelScope.launch {
+            setState {
+                state.value.copy(
+                    codeIds = codeIds,
+                )
+            }
         }
     }
 }
 
 @Immutable
 internal data class InterestsState(
-    val interestsList: InterestsEntity?,
+    val interestsMajorList: InterestsEntity?,
+    val interestsRecruitments: InterestsRecruitmentsEntity?,
+    val codeIds: List<Int>,
+    val majorId: Int?,
 ) {
     companion object {
         fun getInitialState() = InterestsState(
-            interestsList = null,
+            interestsMajorList = null,
+            interestsRecruitments = null,
+            codeIds = emptyList(),
+            majorId = null,
         )
     }
 }
+
 internal sealed class InterestsSideEffect
