14b26dd8361f904cc5a979acadc888d138b17e4b
유섭 <yoonbk2001027@gmail.com>
2025-05-09 11:11:25 +0900

    refactor :: 관심 분야 데이터 모델 구조 변경

--- core/domain/src/main/java/team/retum/usecase/entity/interests/InterestsEntity.kt
+++ core/domain/src/main/java/team/retum/usecase/entity/interests/InterestsEntity.kt
@@ -5,13 +5,23 @@ import team.retum.network.model.response.interests.FetchInterestsResponse
 
 @Immutable
 data class InterestsEntity(
-    val id: Int,
-    val studentId: Int,
-    val code: Int,
-    val keyword: String,
-)
+    val studentName: String,
+    val interests: List<InterestMajorEntity>,
+) {
+    data class InterestMajorEntity(
+        val id: Int,
+        val studentId: Int,
+        val code: Int,
+        val keyword: String,
+    )
+}
 
 internal fun FetchInterestsResponse.toEntity() = InterestsEntity(
+    studentName = this.studentName,
+    interests = this.interests.map { it.toEntity() },
+)
+
+private fun FetchInterestsResponse.InterestMajor.toEntity() = InterestsEntity.InterestMajorEntity(
     id = this.id,
     studentId = this.studentId,
     code = this.code,
--- core/network/src/main/java/team/retum/network/model/response/interests/FetchInterestsResponse.kt
+++ core/network/src/main/java/team/retum/network/model/response/interests/FetchInterestsResponse.kt
@@ -5,8 +5,13 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class FetchInterestsResponse(
-    @Json(name = "id") val id: Int,
-    @Json(name = "student_id") val studentId: Int,
-    @Json(name = "code") val code: Int,
-    @Json(name = "keyword") val keyword: String,
-)
+    @Json(name = "student_name") val studentName: String,
+    @Json(name = "interests") val interests: List<InterestMajor>,
+) {
+    data class InterestMajor(
+        @Json(name = "id") val id: Int,
+        @Json(name = "student_id") val studentId: Int,
+        @Json(name = "code") val code: Int,
+        @Json(name = "keyword") val keyword: String,
+    )
+}
