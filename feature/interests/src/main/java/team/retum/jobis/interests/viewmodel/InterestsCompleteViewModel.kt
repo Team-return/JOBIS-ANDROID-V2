02457457cb408facebb84cebd54ee59f4462395f
유섭 <yoonbk2001027@gmail.com>
2025-05-19 11:19:48 +0900

    feat :: init interestsComplete viewModel

--- feature/interests/src/main/java/team/retum/jobis/interests/viewmodel/InterestsCompleteViewModel.kt
+++ feature/interests/src/main/java/team/retum/jobis/interests/viewmodel/InterestsCompleteViewModel.kt
@@ -6,19 +6,15 @@ import team.retum.common.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-internal class InterestsCompleteViewModel @Inject constructor(
-
-) : BaseViewModel<InterestsCompleteState, InterestsCompleteSideEffect>(InterestsCompleteState.getInitialState()) {
-
-}
+internal class InterestsCompleteViewModel @Inject constructor() : BaseViewModel<InterestsCompleteState, InterestsCompleteSideEffect>(InterestsCompleteState.getInitialState())
 
 @Immutable
 internal data class InterestsCompleteState(
-    val number: Number
+    val studentName: String,
 ) {
     companion object {
         fun getInitialState() = InterestsCompleteState(
-            number = 0,
+            studentName = "",
         )
     }
 }
