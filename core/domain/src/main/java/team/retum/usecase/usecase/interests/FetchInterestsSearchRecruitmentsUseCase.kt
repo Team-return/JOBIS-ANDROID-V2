69aac0b4e1d8b7bd2abf71307bc7d64c27f3ac18
유섭 <yoonbk2001027@gmail.com>
2025-05-19 11:21:01 +0900

    chore :: ktlint change

--- core/domain/src/main/java/team/retum/usecase/usecase/interests/FetchInterestsSearchRecruitmentsUseCase.kt
+++ core/domain/src/main/java/team/retum/usecase/usecase/interests/FetchInterestsSearchRecruitmentsUseCase.kt
@@ -4,7 +4,7 @@ import team.retum.data.repository.interests.InterestsRepository
 import team.retum.usecase.entity.interests.toEntity
 import javax.inject.Inject
 
-class FetchInterestsSearchRecruitmentUseCase @Inject constructor(
+class FetchInterestsSearchRecruitmentsUseCase @Inject constructor(
     private val interestsRepository: InterestsRepository,
 ) {
     suspend operator fun invoke() = runCatching {
--- feature/interests/src/main/java/team/retum/jobis/interests/navigation/InterestsCompleteNavigation.kt
+++ feature/interests/src/main/java/team/retum/jobis/interests/navigation/InterestsCompleteNavigation.kt
@@ -3,7 +3,6 @@ package team.retum.jobis.interests.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import team.retum.jobis.interests.ui.Interests
 import team.retum.jobis.interests.ui.InterestsComplete
 
 const val NAVIGATION_INTERESTS_COMPLETE = "interestsComplete"
