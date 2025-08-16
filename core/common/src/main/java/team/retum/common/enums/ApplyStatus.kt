d8272ba3c1b218de98a25acf5fa28a28120f9329
유섭 <yoonbk2001027@gmail.com>
2025-08-16 19:05:51 +0900

    feat :: 지원서 서류 상태 추가

--- core/common/src/main/java/team/retum/common/enums/ApplyStatus.kt
+++ core/common/src/main/java/team/retum/common/enums/ApplyStatus.kt
@@ -3,10 +3,12 @@ package team.retum.common.enums
 enum class ApplyStatus(val value: String) {
     REQUESTED("승인 요청"),
     APPROVED("승인"),
+    SEND("전송"),
     FAILED("탈락"),
     PASS("합격"),
     REJECTED("반려"),
     FIELD_TRAIN("현장실습"),
-    SEND("지원 중"),
     ACCEPTANCE("근로계약"),
+    DOC_FAILED("서류 탈락"),
+    PROCESSING("진행중")
 }
