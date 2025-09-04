89d5dc73e270c3ed03903055e23e1e6c9a9cdc1a
유섭 <yoonbk2001027@gmail.com>
2025-09-04 22:17:00 +0900

    feat :: SearchReview 퍼블리싱 완료

--- feature/review/src/main/java/team/retum/review/ui/review_read/SearchReviewsScreen.kt
+++ feature/review/src/main/java/team/retum/review/ui/review_read/SearchReviewsScreen.kt
@@ -1,14 +1,35 @@
 package team.retum.review.ui.review_read
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import team.retum.jobisdesignsystemv2.appbar.JobisSmallTopAppBar
+import team.retum.jobisdesignsystemv2.foundation.JobisIcon
+import team.retum.jobisdesignsystemv2.textfield.JobisTextField
 
 @Composable
-internal fun SearchReviews() {
-    SearchReviewsScreen()
+internal fun SearchReviews(
+    onBackPressed: () -> Unit,
+) {
+    SearchReviewsScreen(onBackPressed = onBackPressed)
 }
 
 @Composable
-private fun SearchReviewsScreen() {
-    Text("SearchReviewsScreen")
+
+private fun SearchReviewsScreen(
+    onBackPressed: () -> Unit,
+    onNameChange: (String) -> Unit = {},
+) {
+    Column {
+        JobisSmallTopAppBar(
+            onBackPressed = onBackPressed
+        )
+        JobisTextField(
+            value = { "" },
+            hint = "직무를 선택해주세요",
+            onValueChange = onNameChange,
+            drawableResId = JobisIcon.Search,
+        )
+    }
 }
