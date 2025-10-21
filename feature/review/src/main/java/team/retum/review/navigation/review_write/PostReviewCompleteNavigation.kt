37fa66330cd76f51bf00553e1d7911803161d9c1
유섭 <yoonbk2001027@gmail.com>
2025-10-21 11:52:51 +0900

    feat :: postReviewComplete 파일 생성

---
+++ feature/review/src/main/java/team/retum/review/navigation/review_write/PostReviewCompleteNavigation.kt
@@ -0,0 +1,28 @@
+package team.retum.review.navigation.review_write
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import team.retum.common.utils.ResourceKeys
+import team.retum.review.model.PostReviewData
+import team.retum.review.ui.review_write.PostReview
+import team.retum.review.ui.review_write.PostReviewComplete
+import team.retum.review.ui.review_write.PostReviewCompleteScreen
+
+const val NAVIGATION_POST_REVIEW_COMPLETE = "postReviewComplete"
+
+fun NavGraphBuilder.postReviewComplete(
+    onBackPressed: () -> Unit,
+) {
+    composable(
+        route = NAVIGATION_POST_REVIEW_COMPLETE,
+    ) {
+        PostReviewComplete(onBackPressed = onBackPressed)
+    }
+}
+
+fun NavController.navigateToPostReviewComplete() {
+    navigate(NAVIGATION_POST_REVIEW_COMPLETE)
+}
---
+++ feature/review/src/main/java/team/retum/review/ui/review_write/PostReviewCompleteScreen.kt
@@ -0,0 +1,19 @@
+package team.retum.review.ui.review_write
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PostReviewComplete(
+    onBackPressed: () -> Unit,
+) {
+    PostReviewCompleteScreen(
+        onBackPressed = onBackPressed,
+    )
+}
+
+@Composable
+fun PostReviewCompleteScreen(
+    onBackPressed: () -> Unit,
+) {
+
+}
