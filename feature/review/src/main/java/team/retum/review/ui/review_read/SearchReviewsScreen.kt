402471c03b90313d96df0568d3a72abdf3966d72
유섭 <yoonbk2001027@gmail.com>
2025-10-21 15:06:38 +0900

    feat :: search review 결과 ui 적용

--- feature/review/src/main/java/team/retum/review/ui/review_read/SearchReviewsScreen.kt
+++ feature/review/src/main/java/team/retum/review/ui/review_read/SearchReviewsScreen.kt
@@ -1,6 +1,7 @@
 package team.retum.review.ui.review_read
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
@@ -8,14 +9,17 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import team.retum.jobis.review.R
 import team.retum.jobisdesignsystemv2.appbar.JobisSmallTopAppBar
+import team.retum.jobisdesignsystemv2.empty.EmptyContent
 import team.retum.jobisdesignsystemv2.foundation.JobisIcon
 import team.retum.jobisdesignsystemv2.textfield.JobisTextField
+import team.retum.review.ui.component.ReviewItems
 import team.retum.review.viewmodel.SearchReviewsState
 import team.retum.review.viewmodel.SearchReviewsViewModel
 
 @Composable
 internal fun SearchReviews(
     onBackPressed: () -> Unit,
+    onReviewDetailClick: (Long) -> Unit,
     searchViewModel: SearchReviewsViewModel = hiltViewModel(),
 ) {
     val state by searchViewModel.state.collectAsStateWithLifecycle()
@@ -23,7 +27,8 @@ internal fun SearchReviews(
     SearchReviewsScreen(
         state = state,
         onBackPressed = onBackPressed,
-        onNameChange = searchViewModel::setName,
+        onReviewDetailClick = onReviewDetailClick,
+        onNameChange = searchViewModel::setKeyword,
     )
 }
 
@@ -31,6 +36,7 @@ internal fun SearchReviews(
 private fun SearchReviewsScreen(
     state: SearchReviewsState,
     onBackPressed: () -> Unit,
+    onReviewDetailClick: (Long) -> Unit,
     onNameChange: (String) -> Unit,
 ) {
     Column {
@@ -38,10 +44,30 @@ private fun SearchReviewsScreen(
             onBackPressed = onBackPressed
         )
         JobisTextField(
-            value = { state.name ?: "" },
+            value = { state.keyword ?: "" },
             hint = stringResource(R.string.review_hint),
             onValueChange = onNameChange,
             drawableResId = JobisIcon.Search,
         )
+        if (!state.showRecruitmentsEmptyContent) {
+            LazyColumn {
+                items(state.reviews.size) {
+                    val review = state.reviews[it]
+                    ReviewItems(
+                        companyImageUrl = review.companyLogoUrl,
+                        companyName = review.companyName,
+                        reviewId = review.reviewId.toLong(),
+                        writer = review.writer,
+                        major = review.major,
+                        onReviewDetailClick = onReviewDetailClick,
+                    )
+                }
+            }
+        } else {
+            EmptyContent(
+                title = stringResource(R.string.search_review_not_find),
+                description = stringResource(R.string.search_review_expect)
+            )
+        }
     }
 }
--- feature/review/src/main/java/team/retum/review/viewmodel/SearchReviewsViewModel.kt
+++ feature/review/src/main/java/team/retum/review/viewmodel/SearchReviewsViewModel.kt
@@ -1,26 +1,81 @@
 package team.retum.review.viewmodel
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.key
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import team.retum.common.base.BaseViewModel
+import team.retum.usecase.entity.FetchReviewsEntity
+import team.retum.usecase.usecase.review.FetchReviewsUseCase
 import javax.inject.Inject
+
+const val SEARCH_DEBOUNCE_MILLIS = 1000L
 
 @HiltViewModel
 internal class SearchReviewsViewModel @Inject constructor(
+    private val fetchReviewsUseCase: FetchReviewsUseCase,
 ) : BaseViewModel<SearchReviewsState, Unit>(SearchReviewsState.getInitialState()) {
 
-    internal fun setName(name: String) = setState {
-        state.value.copy(name = name)
+    init {
+        debounceName()
+    }
+
+    internal fun setKeyword(keyword: String) = setState {
+        state.value.copy(keyword = keyword)
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun debounceName() {
+        viewModelScope.launch {
+            state.map { it.keyword }.distinctUntilChanged().debounce(SEARCH_DEBOUNCE_MILLIS).collect {
+                if (!it.isNullOrBlank()) {
+                    fetchReviews()
+                }
+            }
+        }
+    }
+
+    internal fun fetchReviews() {
+        with(state.value) {
+            viewModelScope.launch(Dispatchers.IO) {
+                fetchReviewsUseCase(
+                    companyId = null,
+                    page = null,
+                    location = null,
+                    interviewType = null,
+                    keyword = keyword,
+                    year = null,
+                    code = null
+                ).onSuccess {
+                    setState {
+                        state.value.copy(
+                            showRecruitmentsEmptyContent = it.reviews.isEmpty(),
+                            reviews = it.reviews,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
 @Immutable
 data class SearchReviewsState(
-    val name: String?
+    val keyword: String?,
+    val reviews: List<FetchReviewsEntity.Review>,
+    val showRecruitmentsEmptyContent: Boolean
 ) {
     companion object {
         fun getInitialState() = SearchReviewsState(
-            name = null,
+            keyword = null,
+            reviews = emptyList(),
+            showRecruitmentsEmptyContent = false,
         )
     }
 }
