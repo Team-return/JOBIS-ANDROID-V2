441de8cb63923d48249b3690618ab0f1e43fdf15
유섭 <yoonbk2001027@gmail.com>
2025-10-23 11:26:09 +0900

    feat :: myReview UseCase 구현

--- core/network/src/main/java/team/retum/network/model/response/FetchMyReviewResponse.kt
+++ core/network/src/main/java/team/retum/network/model/response/FetchMyReviewResponse.kt
@@ -5,10 +5,10 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class FetchMyReviewResponse(
-    @Json(name = "reviews") val reviews: List<Review>
+    @Json(name = "reviews") val reviews: List<MyReview>
 ) {
     @JsonClass(generateAdapter = true)
-    data class Review(
+    data class MyReview(
         @Json(name = "review_id") val reviewId: Long,
         @Json(name = "company_name") val companyName: String,
     )
