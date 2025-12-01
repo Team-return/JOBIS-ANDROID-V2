b969450b13dc3beefccb478b8b714f5d29dd14d0
유섭 <yoonbk2001027@gmail.com>
2025-12-01 23:48:57 +0900

    chore :: ktlintCheck

--- feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
+++ feature/signin/src/main/java/team/retum/signin/ui/SignInScreen.kt
@@ -10,10 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
