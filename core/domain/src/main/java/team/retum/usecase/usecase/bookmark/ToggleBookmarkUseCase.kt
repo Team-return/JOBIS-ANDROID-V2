18c14cc6cc0424a754e9aade7df5d252a9249bc7
유섭 <yoonbk2001027@gmail.com>
2025-12-31 10:50:38 +0900

    refactor :: usecase bookmarkLocalEntity로 수정

--- core/domain/src/main/java/team/retum/usecase/usecase/bookmark/ToggleBookmarkUseCase.kt
+++ core/domain/src/main/java/team/retum/usecase/usecase/bookmark/ToggleBookmarkUseCase.kt
@@ -1,12 +1,14 @@
 package team.retum.usecase.usecase.bookmark
 
 import team.retum.data.repository.bookmark.BookmarkRepository
+import team.retum.jobis.local.entity.BookmarkLocalEntity
+import team.retum.usecase.entity.BookmarksEntity
 import javax.inject.Inject
 
 class ToggleBookmarkUseCase @Inject constructor(
     private val bookmarkRepository: BookmarkRepository,
 ) {
-    suspend operator fun invoke(recruitmentId: Long): Result<Unit> {
+    suspend operator fun invoke(recruitmentId: BookmarkLocalEntity): Result<Unit> {
         return bookmarkRepository.toggleBookmark(recruitmentId)
     }
 }
