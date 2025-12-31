34d0a0a77ee06381c75359c8729fce076a206920
유섭 <yoonbk2001027@gmail.com>
2025-12-31 10:50:07 +0900

    refactor :: entity 수정

--- core/domain/src/main/java/team/retum/usecase/entity/BookmarksEntity.kt
+++ core/domain/src/main/java/team/retum/usecase/entity/BookmarksEntity.kt
@@ -22,7 +22,7 @@ fun BookmarksResponse.toBookmarkEntity() = BookmarksEntity(
 )
 
 private fun BookmarksResponse.BookmarkResponse.toEntity() = BookmarksEntity.BookmarkEntity(
-    companyLogoUrl = ResourceKeys.IMAGE_URL + this.companyLogoUrl,
+    companyLogoUrl = normalizeLogoUrl(this.companyLogoUrl),
     companyName = this.companyName,
     recruitmentId = this.recruitmentId,
     createdAt = this.createdAt.substring(0..9),
@@ -31,8 +31,16 @@ private fun BookmarksResponse.BookmarkResponse.toEntity() = BookmarksEntity.Book
 fun List<BookmarkLocalEntity>.toBookmarkEntities() = map { it.toLocalEntity() }
 
 private fun BookmarkLocalEntity.toLocalEntity() = BookmarksEntity.BookmarkEntity(
-    companyLogoUrl = ResourceKeys.IMAGE_URL + companyLogoUrl,
+    companyLogoUrl = normalizeLogoUrl(companyLogoUrl),
     companyName = companyName,
     recruitmentId = recruitmentId,
     createdAt = if (createdAt.length >= 10) createdAt.substring(0..9) else createdAt,
 )
+
+private fun normalizeLogoUrl(logoUrl: String): String {
+    return if (logoUrl.startsWith(ResourceKeys.IMAGE_URL)) {
+        logoUrl
+    } else {
+        ResourceKeys.IMAGE_URL + logoUrl
+    }
+}
