8cdd386352fab333b5094f72e72ab79e015f7893
유섭 <yoonbk2001027@gmail.com>
2026-02-04 10:38:14 +0900

    feat :: 면접 일정 조회 구현

--- feature/interview-schedule/src/main/java/team/retum/jobis/interview/schedule/viewmodel/InterviewScheduleViewModel.kt
+++ feature/interview-schedule/src/main/java/team/retum/jobis/interview/schedule/viewmodel/InterviewScheduleViewModel.kt
@@ -1,23 +1,47 @@
 package team.retum.jobis.interview.schedule.viewmodel
 
 import androidx.compose.runtime.Immutable
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import team.retum.common.base.BaseViewModel
+import team.retum.usecase.entity.interview.InterviewsEntity
+import team.retum.usecase.usecase.interview.FetchInterviewUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 internal class InterviewScheduleViewModel @Inject constructor(
-    
+    private val fetchInterviewUseCase: FetchInterviewUseCase,
 ) : BaseViewModel<InterviewScheduleState, InterviewScheduleSideEffect>(InterviewScheduleState.getInitialState()) {
+
+    init {
+        fetchInterviews()
+    }
+
+    private fun fetchInterviews() {
+        viewModelScope.launch(Dispatchers.IO) {
+            fetchInterviewUseCase().onSuccess {
+                setState {
+                    state.value.copy(
+                        interviews = it.interviews,
+                        totalCount = it.totalCount,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Immutable
 internal data class InterviewScheduleState(
-    val placeholder: String,
+    val interviews: List<InterviewsEntity.InterviewEntity>,
+    val totalCount: Int,
 ) {
     companion object {
         fun getInitialState() = InterviewScheduleState(
-            placeholder = "",
+            interviews = emptyList(),
+            totalCount = 0,
         )
     }
 }
