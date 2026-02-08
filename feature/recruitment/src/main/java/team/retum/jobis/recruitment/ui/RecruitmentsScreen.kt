599af3665f3bbf50dca7721a0d675ad2e10da59c
ashxom <anseha090115@naver.com>
2026-02-08 22:38:11 +0900

    feat :: 모집의뢰서 정렬 드롭다운 UI 추가

--- feature/recruitment/src/main/java/team/retum/jobis/recruitment/ui/RecruitmentsScreen.kt
+++ feature/recruitment/src/main/java/team/retum/jobis/recruitment/ui/RecruitmentsScreen.kt
@@ -1,10 +1,12 @@
 package team.retum.jobis.recruitment.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -23,6 +25,22 @@ import team.retum.jobisdesignsystemv2.foundation.JobisIcon
 import team.retum.jobisdesignsystemv2.foundation.JobisTheme
 import team.retum.jobisdesignsystemv2.toast.JobisToast
 import team.retum.usecase.entity.RecruitmentsEntity
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.collections.immutable.persistentListOf
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+
 
 @Composable
 internal fun Recruitments(
@@ -78,11 +96,20 @@ private fun RecruitmentsScreen(
     whetherFetchNextPage: (lastVisibleItemIndex: Int) -> Boolean,
     fetchNextPage: () -> Unit,
 ) {
+    var sortExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    var selectedSort by remember {
+        mutableStateOf("기본순")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(JobisTheme.colors.background),
     ) {
+
         JobisLargeTopAppBar(title = stringResource(id = R.string.recruitment)) {
             JobisIconButton(
                 drawableResId = JobisIcon.Filter,
@@ -104,4 +131,78 @@ private fun RecruitmentsScreen(
             fetchNextPage = fetchNextPage,
         )
     }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 24.dp, top = 72.dp, end = 24.dp, bottom = 8.dp),
+        horizontalArrangement = Arrangement.End,
+    ) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.clickable {
+            sortExpanded = true
+        }
+    )
+    {
+        Text(
+            text = "$selectedSort ▾ ",
+            maxLines = 1,
+        )
+        DropdownMenu(
+            expanded = sortExpanded,
+            onDismissRequest = { sortExpanded = false },
+        ) {
+            DropdownMenuItem(
+                text = { Text("기본순") },
+                onClick = {
+                    selectedSort = "기본순"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("매출") },
+                onClick = {
+                    selectedSort = "매출"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("직원 ↓") },
+                onClick = {
+                    selectedSort = "직원 ↓"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("직원 ↑") },
+                onClick = {
+                    selectedSort = "직원 ↑"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("공고마감 ↓") },
+                onClick = {
+                    selectedSort = "공고마감 ↓"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("공고마감 ↑") },
+                onClick = {
+                    selectedSort = "공고마감 ↑"
+                    sortExpanded = false
+                    // TODO : viewModel에 정렬할 값 전달할 수 있게
+                }
+            )
+        }
+    }
+    }
+
 }
