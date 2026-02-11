7d60bd398b4aa867822ccad8ae75495aeb7377bb
ashxom <anseha090115@naver.com>
2026-02-12 08:42:20 +0900

    refactor :: RecruitSortType 단순 enum 형태로 수정

--- core/common/src/main/java/team/retum/common/enums/RecruitSortType.kt
+++ core/common/src/main/java/team/retum/common/enums/RecruitSortType.kt
@@ -1,9 +1,10 @@
 package team.retum.common.enums
 
-enum class RecruitSortType(val apiValue: String) {
-    TAKE("TAKE"),
-    WORKERS_COUNT_DESC("WORKERS_COUNT_DESC"),
-    WORKERS_COUNT_ASC("WORKERS_COUNT_ASC"),
-    DEADLINE_DESC("DEADLINE_DESC"),
-    DEADLINE_ASC("DEADLINE_ASC");
+enum class RecruitSortType{
+    TAKE,
+    WORKERS_COUNT_DESC,
+    WORKERS_COUNT_ASC,
+    DEADLINE_DESC,
+    DEADLINE_ASC,
 }
+
