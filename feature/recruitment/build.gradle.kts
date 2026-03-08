0173f8c4ef3daf095363144f7a06096b3ca3c7aa
ashxom <anseha090115@naver.com>
2026-03-08 20:24:07 +0900

    refactor :: 필요 없는 preview 코드 삭제

--- feature/recruitment/build.gradle.kts
+++ feature/recruitment/build.gradle.kts
@@ -25,7 +25,5 @@ dependencies {
     implementation(libs.coil.compose)
 
     implementation(libs.kotlinx.collections.immutable)
-
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
 }
+
