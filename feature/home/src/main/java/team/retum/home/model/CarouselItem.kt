cc001ab0232ee5f3fcce97f1e543ac02f9f6011b
ashxom <anseha090115@naver.com>
2026-03-21 23:44:05 +0900

    fix :: 지원 여부 필드 추가

--- feature/home/src/main/java/team/retum/home/model/CarouselItem.kt
+++ feature/home/src/main/java/team/retum/home/model/CarouselItem.kt
@@ -3,6 +3,6 @@ package team.retum.home.model
 data class CarouselItem(
     val id: Long,
     val name: String,
-    val description: String,
     val logoUrl: String,
+    val isRecruiting: Boolean,
 )
